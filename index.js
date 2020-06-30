11db7abf9ebd05a5eb77a321e0aae17c570e1770
Arne Strohmeier <arne@rpkak.de>
2020-06-30 12:35:18 +0200

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Client } = require("discord.js");
+
+while(true){
+	const client = new Client();
+
+	console.log('register');
+
+	client.on("ready", () => {
+		console.log("Ready");
+		//member.roles.add(member.guild.roles.cache.find(r => r.name === "[S] Spieler"));
+	});
+
+	client.on("guildMemberAdd", (member) => {
+		var fName = `[S] ${member.user.username}`;
+		console.log(fName)
+		member.setNickname(fName);
+		//member.roles.add(member.guild.roles.cache.find(r => r.name === "[S] Spieler"));
+	});
+
+	console.log('start');
+	client.login(process.env.TOKEN);
+	console.log('restart');
+}

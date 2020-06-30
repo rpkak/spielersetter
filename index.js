67e24e331b0176b2a3cee4417b6b26db07d5105b
Arne Strohmeier <arne@rpkak.de>
2020-06-30 13:10:09 +0200

    unwhile

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const { Client } = require("discord.js");
 
-while(true){
-	const client = new Client();
+//while(true){
+const client = new Client();
 
-	console.log('register');
+console.log('register');
 
-	client.on("ready", () => {
-		console.log("Ready");
-		//member.roles.add(member.guild.roles.cache.find(r => r.name === "[S] Spieler"));
-	});
+client.on("ready", () => {
+	console.log("Ready");
+	//member.roles.add(member.guild.roles.cache.find(r => r.name === "[S] Spieler"));
+});
 
-	client.on("guildMemberAdd", (member) => {
-		var fName = `[S] ${member.user.username}`;
-		console.log(fName)
-		member.setNickname(fName);
-		//member.roles.add(member.guild.roles.cache.find(r => r.name === "[S] Spieler"));
-	});
+client.on("guildMemberAdd", (member) => {
+	var fName = `[S] ${member.user.username}`;
+	console.log(fName)
+	member.setNickname(fName);
+	//member.roles.add(member.guild.roles.cache.find(r => r.name === "[S] Spieler"));
+});
 
-	console.log('start');
-	client.login(process.env.token);
-	console.log('restart');
-}
+console.log('start');
+client.login(process.env.token);
+console.log('stop');
+//}
